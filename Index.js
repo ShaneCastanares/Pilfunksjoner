e04a38a88d8b0a35a3bab15eae76c74ed4998b18
Shane Castanares <shane.@Shanes-MacBook-Air.local>
2022-03-30 08:31:57 +0200

    Adding assignment file

---
+++ Index.js
@@ -0,0 +1,25 @@
+// Oppgave 1
+const hei = () =>{
+    console.log("Hello World!");
+}
+hei();
+
+// Oppgave 2
+const nah = Jesus =>{
+    console.log("nah " + Jesus);
+}
+nah("Jesus finnnes ikkkkeee");
+
+// Oppgave 3
+
+const klassen =(hei, på, deg)=>{
+    console.log("Yallah " + hei + ", " + på + " og " + deg);
+}
+klassen("JESUS" , "MO" , "KRISTUS");
+
+// oppgave 4
+// -----
+
+
+// Oppgave 5
+// Ting blir ikke hoista med Pilfunksjoner, men det blir det med vanlig Funskjoner
